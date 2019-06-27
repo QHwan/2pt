385cf38895944257c2efd18300242599d4303ffc
QHwan <poter1026@gmail.com>
2019-06-27 16:48:58 +0900

    fourier

--- plot.py
+++ plot.py
@@ -1,7 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-imat = np.loadtxt("vac.xvg")
+imat = np.loadtxt("dos.xvg")
 plt.plot(imat[:,0], imat[:,1])
 plt.plot(imat[:,0], imat[:,2])
+plt.plot(imat[:,0], imat[:,1] + imat[:,2])
 plt.show()
