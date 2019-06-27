e4cef2c563cb96ad2955ec5193cc88024b247d84
QHwan <poter1026@gmail.com>
2019-06-27 14:14:06 +0900

    add

---
+++ mvacf_water.py
@@ -0,0 +1,16 @@
+# calculate the hb_density(r)
+# input file :
+#		water_coordinate_file, water_com_coordinate_file, hbmap_file, hbindex_file
+
+import numpy as np 
+import sys
+import math
+import time
+from mvacf_water import main
+
+start = time.time()
+
+main()
+
+end = time.time() - start
+print end
